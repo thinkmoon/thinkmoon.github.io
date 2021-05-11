775d6d4661094d2e69d95a7e69d8ea4da756832d
醉月思 <2571255509@qq.com>
2021-05-11 23:23:58 +0800

    增加TS支持

---
+++ docs/_nuxt/static/1620744781/post/843/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620744781",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002F843",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
