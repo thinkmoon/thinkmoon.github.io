5ed48c4e0a4308b704a6a972f4be0a77bf96825d
醉月思 <tim@thinkmoon.cn>
2022-01-04 23:38:03 +0800

    修改https请求

--- api/request.ts
+++ api/request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-axios.defaults.baseURL = "http://service.thinkmoon.cn/api";
+axios.defaults.baseURL = "https://service.thinkmoon.cn/api";
 
 function request(options: AxiosRequestConfig) {
   return new Promise(resolve => {
