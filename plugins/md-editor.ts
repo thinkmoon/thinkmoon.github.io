feb43fe5c1be50e3a4c44d490fd40ef5fc0ddc41
Annabel3722 <limanshi1997@126.com>
2022-02-25 19:29:54 +0800

    修复editor问题

---
+++ plugins/md-editor.ts
@@ -0,0 +1,17 @@
+import { defineNuxtPlugin } from '#app'
+
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+// highlightjs
+import hljs from 'highlight.js';
+
+VMdEditor.use(githubTheme, {
+  Hljs: hljs,
+});
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VMdEditor);
+})
