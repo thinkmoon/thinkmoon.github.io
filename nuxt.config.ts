e7228942fed6144d08cbad1cae0321744ef4a9e0
醉月思 <tim@thinkmoon.cn>
2022-03-05 13:19:52 +0800

    优化细节

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
 import { defineNuxtConfig } from 'nuxt3';
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
 
+const lifecycle = process.env.npm_lifecycle_event;
 export default defineNuxtConfig({
-  css: [
-    'assets/css/index.less',
-  ],
+
   meta: {
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: "description",
+        name: "description",
+        content: "ElementPlus + Nuxt3",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     script: [
       {
         src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
@@ -12,5 +22,54 @@ export default defineNuxtConfig({
         'data-ad-client': 'ca-pub-3208634444966567'
       }
     ]
-  }
+  },
+  css: [
+    'assets/css/index.less',
+  ],
+
+  // build
+  build: {
+    transpile: lifecycle === 'build' ? ['element-plus'] : [],
+  },
+
+  // build modules
+  buildModules: [
+    '@pinia/nuxt',
+    '@nuxtjs/svg',
+    '@vueuse/nuxt',
+    'nuxt-windicss',
+    'unplugin-icons/nuxt',
+  ],
+
+  // auto import components
+  components: true,
+
+  // vite plugins
+  vite: {
+    plugins: [
+      Components({
+        dts: true,
+        resolvers: [IconsResolver({})],
+      }),
+    ],
+  },
+
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+
+  // windicss
+  windicss: {
+    analyze: {
+      analysis: {
+        interpretUtilities: false,
+      },
+      server: {
+        port: 4000,
+        open: false,
+      },
+    },
+    scan: true,
+  },
 });
